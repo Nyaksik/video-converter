5b9bb005cfe826c05be4386429fb6c6f67cfc01a
Maksim Kaporikov <makapori@dazlab.io>
2026-02-04 14:39:48 +0700

    faat: sitemap, robots.txt, compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, envField } from 'astro/config'
+import vue from '@astrojs/vue'
+import tailwindcss from '@tailwindcss/vite'
+import robotsTxt from 'astro-robots-txt'
+import sitemap from '@astrojs/sitemap'
+import { loadEnv } from 'vite'
+
+import compressor from 'astro-compressor';
+
+const { PUBLIC_SITE_URL, PUBLIC_SITE_NAME } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: PUBLIC_SITE_URL,
+  env: {
+    schema: {
+      PUBLIC_SITE_NAME: envField.string({ context: 'server', access: 'public', default: PUBLIC_SITE_NAME })
+    }
+  },
+  integrations: [vue(), robotsTxt(), sitemap(), compressor()],
   vite: {
     optimizeDeps: {
       exclude: [
